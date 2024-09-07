52a0929eed2fbfc36b9793bd88f922e58e210898
RINSHID CH <rinshidch10@gmail.com>
2024-09-07 23:09:15 +0530

    Add files via upload

---
+++ changedate.dart
@@ -0,0 +1,10 @@
+// Write a Dart program to display the rearranged date.
+// // Eg: List date = [12,5,2023]. To arrangeddate = 12/5/2023
+void main() {
+  List date = [12, 5, 2023];
+  int day = 12;
+  int month = 5;
+  int year = 2023;
+  String gett = '$day/$month/$year';
+  print(gett);
+}
