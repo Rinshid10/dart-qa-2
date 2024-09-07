bd6da4959dff3190a0b58be70829beb9cb50d0f0
RINSHID CH <rinshidch10@gmail.com>
2024-09-07 23:09:43 +0530

    Add files via upload

---
+++ midchar.dart
@@ -0,0 +1,29 @@
+// String word1 = "safvan";
+// String middleChar1 = middleCharacter(word1);
+// print(middleChar1);
+import 'dart:io';
+
+void main() {
+
+String name = "rinshid"; 
+ 
+
+  
+  int a = name.length ~/ 2;
+  var b = name.substring(a - 1, a +1);
+  print(b);
+
+     
+        //  <<<<<<<<while using function >>>>>>
+  
+
+  // String named(String gett) {
+  //   int nam = gett.length ~/ 2;
+  //   var a = gett.substring(nam - 1, nam + 1);
+  //   return a;
+  // }
+
+  // print(named(name));
+
+  
+}
