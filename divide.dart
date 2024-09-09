b6545d22cd5394974a3a53ee495d271cf3d68c96
RINSHID CH <rinshidch10@gmail.com>
2024-09-09 11:15:09 +0530

    Add files via upload

---
+++ divide.dart
@@ -0,0 +1,9 @@
+void main() {
+//       Input: dividend = 10, divisor = 3
+// Output: 3
+// Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+  int add = 10;
+  int added = 10 ~/ 3;
+  print(added);
+}
