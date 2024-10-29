cddfdf61b312e72a72f1ede210fc77247316f33d
RINSHID CH <rinshidch10@gmail.com>
2024-10-29 20:22:29 +0530

    Add files via upload

---
+++ capitalName.dart
@@ -0,0 +1,10 @@
+void main() {
+//  “my name is khan” ➞ “myNameIsKhan
+  var a = 'my name is khan';
+  var c = a.split(' ');
+  String name = '';
+  for (var i = 0; i < c.length; i++) {
+    c[i] = c[i][0].toUpperCase() + c[i].substring(1);
+  }
+  print(c.join());
+}
